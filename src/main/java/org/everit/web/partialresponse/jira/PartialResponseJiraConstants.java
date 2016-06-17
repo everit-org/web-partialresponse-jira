bfa87e46921feaadf2d71caf9aff93c570f9c3f2
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2016-06-17 16:02:37 +0200

    Release 1

---
+++ src/main/java/org/everit/web/partialresponse/jira/PartialResponseJiraConstants.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2011 Everit Kft. (http://www.everit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.everit.web.partialresponse.jira;
+
+/**
+ * Constants of the Jira Plugin port of Partial Response.
+ *
+ */
+public final class PartialResponseJiraConstants {
+
+  /**
+   * The name of the context that should be specified to add the partialresponse javascript file to
+   * the page.
+   */
+  public static final String CONTEXT = "org.everit.web.partialresponse";
+
+  private PartialResponseJiraConstants() {
+  }
+}
